f45113ef87e9a36730a803a5e8b3df2860d333e6
Rohit Jana <140490171+Rohitjana9098@users.noreply.github.com>
2026-07-28 12:51:37 +0530

    Time: 315 ms (6.56%), Space: 68.7 MB (66.42%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    # Filter rows based on area OR population criteria
     is_big = (world['area'] >= 3000000) | (world['population'] >= 25000000)
-
-    return world[is_big][['name','population','area']]
+    
+    # Return selected columns
+    return world[is_big][['name', 'population', 'area']]
