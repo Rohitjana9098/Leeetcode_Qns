6d46f362eb470469c5fc5f9e122848b0634a405f
Rohit Jana <140490171+Rohitjana9098@users.noreply.github.com>
2026-09-26 20:39:36 +0530

    Time: 1 ms (87.1%), Space: 44.4 MB (20.64%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,28 +2,32 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
 
-        backtrack(0, nums, curr, ans);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
-        return ans;
+        generateSubsets(nums, 0, temp, result);
+
+        return result;
     }
 
-    public void backtrack(int index, int[] nums,List<Integer> curr,List<List<Integer>> ans) {
+    private void generateSubsets(int[] nums, int idx,List<Integer> temp,List<List<Integer>> result) {
 
-        // Store the current subset
-        ans.add(new ArrayList<>(curr));
-
-        for (int i = index; i < nums.length; i++) {
-            // Choose
-            curr.add(nums[i]);
-
-            // Explore
-            backtrack(i + 1, nums, curr, ans);
-
-            // Undo the choice
-            curr.remove(curr.size() - 1);
+        // Base case
+        if (idx == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
         }
+
+        // Choice 1: Exclude the current element
+        generateSubsets(nums, idx + 1, temp, result);
+
+        // Choice 2: Include the current element
+        temp.add(nums[idx]);
+
+        generateSubsets(nums, idx + 1, temp, result);
+
+        // Backtrack
+        temp.remove(temp.size() - 1);
     }
 }
